62261d901878d3fdb9ad938582da6e28b71ec8c4
Rafael Bachmann <triolenzunge@gmail.com>
2016-06-18 14:24:11 +0200

    implement shortmessage decoding

--- decoders/SeparateTrackDecoder.java
+++ decoders/SeparateTrackDecoder.java
@@ -1,6 +1,7 @@
 package decoders;
 
 import javax.sound.midi.Sequence;
+import javax.sound.midi.ShortMessage;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
@@ -9,17 +10,23 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+import static decoders.midiUtil.getKeyName;
+
 /**
  * Created by ra on 18.06.16.
- * Part of midiParser, in package PACKAGE_NAME.
+ * Part of midiParser, in package decoders.
  */
-public class SeparateTrackDecoder implements Decoder {
+public class SeparateTrackDecoder implements Decoder<ShortMessage> {
     private Path outputDir = Paths.get("assets/midi/csv/tracks/");
+
+    public SeparateTrackDecoder(Path outputDir) {
+        this.outputDir = Paths.get(outputDir.endsWith("/") ? outputDir + "tracks/" : outputDir + "/tracks/");
+    }
 
     @Override
     public void decode(Sequence sequence, String name) {
         for (int i = 0; i < sequence.getTracks().length; i++) {
-            Path file = Paths.get(outputDir + "track" + i + ".csv");
+            Path file = Paths.get(outputDir + "track_" + i + ".csv");
             List<String> lines = new ArrayList<>();
 
             try {
@@ -28,6 +35,24 @@ public class SeparateTrackDecoder implements Decoder {
                 e.printStackTrace();
             }
         }
+    }
+
+    @Override
+    public String decodeMessage(ShortMessage message) {
+        String strMessage;
+        switch (message.getCommand()) {
+            case 0x80:
+                strMessage = "note Off " + getKeyName(message.getData1()) + ", velocity " + message.getData2();
+                break;
+
+            case 0x90:
+                strMessage = "note On " + getKeyName(message.getData1()) + ", velocity " + message.getData2();
+                break;
+           default:
+                strMessage = "unknown message: status = " + message.getStatus() + "; byte1 = " + message.getData1() + "; byte2 = " + message.getData2();
+                break;
+        }
+        return strMessage;
     }
 
     @Override
